b7533135b5bb2b1e386a6ffe54664356dc83e64d
SteveKunG <mccommander_minecraft@hotmail.com>
2023-12-10 21:05:38 +0700

    Update to 1.20.4

--- src/main/java/com/stevekung/msr/client/renderer/MinecartSpawnerRenderer.java
+++ src/main/java/com/stevekung/msr/client/renderer/MinecartSpawnerRenderer.java
@@ -1,14 +1,13 @@
 package com.stevekung.msr.client.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
 
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.SpawnerRenderer;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MinecartRenderer;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.vehicle.MinecartSpawner;
 
 /**
@@ -31,25 +30,13 @@ public class MinecartSpawnerRenderer extends MinecartRenderer<MinecartSpawner>
     {
         super.render(minecartSpawner, entityYaw, partialTicks, poseStack, bufferSource, packedLight);
         poseStack.pushPose();
+        poseStack.translate(-0.5F, 0.0F, -0.5F);
         var baseSpawner = minecartSpawner.getSpawner();
-        var entity = baseSpawner.getOrCreateDisplayEntity(minecartSpawner.level(), minecartSpawner.level().getRandom(), minecartSpawner.blockPosition());
+        var entity = baseSpawner.getOrCreateDisplayEntity(minecartSpawner.level(), minecartSpawner.blockPosition());
 
         if (entity != null)
         {
-            var f = 0.53125F;
-            var g = Math.max(entity.getBbWidth(), entity.getBbHeight());
-
-            if (g > 1.0)
-            {
-                f /= g;
-            }
-
-            poseStack.translate(0.0F, 0.4F, 0.0F);
-            poseStack.mulPose(Axis.YP.rotationDegrees((float)(Mth.lerp(partialTicks, baseSpawner.getoSpin(), baseSpawner.getSpin()) * 10.0F)));
-            poseStack.translate(0.0F, -0.2F, 0.0F);
-            poseStack.mulPose(Axis.XP.rotationDegrees(-30.0F));
-            poseStack.scale(f, f, f);
-            this.entityRenderer.render(entity, 0.0, 0.0, 0.0, 0.0F, partialTicks, poseStack, bufferSource, packedLight);
+            SpawnerRenderer.renderEntityInSpawner(partialTicks, poseStack, bufferSource, packedLight, entity, this.entityRenderer, baseSpawner.getoSpin(), baseSpawner.getSpin());
         }
 
         poseStack.popPose();
