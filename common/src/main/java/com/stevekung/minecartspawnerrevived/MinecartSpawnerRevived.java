4d7074b310dcd953a3b798eaae700080175aed9f
SteveKunG <mccommander_minecraft@hotmail.com>
2025-11-05 17:09:25 +0700

    chore: update to 25w45a

--- common/src/main/java/com/stevekung/minecartspawnerrevived/MinecartSpawnerRevived.java
+++ common/src/main/java/com/stevekung/minecartspawnerrevived/MinecartSpawnerRevived.java
@@ -2,7 +2,7 @@ package com.stevekung.minecartspawnerrevived;
 
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public class MinecartSpawnerRevived
 {
@@ -12,12 +12,12 @@ public class MinecartSpawnerRevived
     /**
      * Use to request SpawnData from the client side. Then sent the SpawnData into the server.
      */
-    public static final ResourceLocation REQUEST_SPAWNDATA = ResourceLocation.fromNamespaceAndPath(MOD_ID, "request_spawndata");
+    public static final Identifier REQUEST_SPAWNDATA = Identifier.fromNamespaceAndPath(MOD_ID, "request_spawndata");
 
     /**
      * Use to send SpawnData to the client side to set spawner display.
      */
-    public static final ResourceLocation SEND_SPAWNDATA = ResourceLocation.fromNamespaceAndPath(MOD_ID, "send_spawndata");
+    public static final Identifier SEND_SPAWNDATA = Identifier.fromNamespaceAndPath(MOD_ID, "send_spawndata");
 
     public static void init()
     {
